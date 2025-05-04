da79067e9125cb481215aa0a28a51900c15e3151
MARINA ZHUKOVA <marishka19882007@yandex.ru>
2025-05-04 18:18:46 +0300

    Исправление задач 1 и 4 финальная работа спринта 2

--- task1.py
+++ task1.py
@@ -5,11 +5,12 @@ class Case:
         self.step_description = step_description
         self.expected_result = expected_result
 
-    def print_test_case_info(self):
+    def print_test_case_info(self): 
         print(f"ID тест-кейса:  {self.test_case_id}"
               f"\nНазвание: {self.name}"
               f"\nОписание шага: {self.step_description}"
               f"\nОжидаемый результат: {self.expected_result}")
+        
 class ExtendedCase(Case):
     def __init__(self, test_case_id, name, step_description, expected_result, precondition, environment):
         self.precondition = precondition
@@ -17,11 +18,11 @@ class ExtendedCase(Case):
         super().__init__(test_case_id, name, step_description, expected_result)
 
     def print_test_case_info(self):
-        print(f"ID тест-кейса:  {self.test_case_id}"
-              f"\nНазвание: {self.name}"
-              f"\nОписание шага: {self.step_description}"
-              f"\nОжидаемый результат: {self.expected_result}"
-              f"\nПредусловие: {self.precondition}"
-              f"\nОкружение: {self.environment}")
-case = ExtendedCase('1', 'Наличие кнопки Принять', '1. Открыть вкладку приёма документов 2. Проверить наличие кнопки ', 'Кнопка доступна', 'Открыть сервис', 'Яндекс Браузер')
+        super().print_test_case_info()
+        print(f"Предусловие: {self.precondition}",
+                f"\nОкружение: {self.environment}")
+        
+        
+case = ExtendedCase('1', 'Наличие кнопки Принять', '1. Открыть вкладку приёма документов 2. Проверить наличие кнопки ',
+                    'Кнопка доступна', 'Открыть сервис', 'Яндекс Браузер')
 ExtendedCase.print_test_case_info(case)
--- task4.py
+++ task4.py
@@ -1,35 +1,31 @@
 class EmployeeSalary:
     hourly_payment = 400
 
-    def __init__(self, name, hours=None, rest_days=0, email=None):
+    def __init__(self, name, hours, rest_days, email):
         self.name = name
         self.hours = hours
         self.rest_days = rest_days
-        self.email = email if email else self.get_email()
+        self.email = email
 
-    def get_hours(self):
-        if self.hours is None:
-            self.hours = (7 - self.rest_days) * 8
-        return self.hours
+    @classmethod
+    def get_hours(cls, name, rest_days, email):
+        if cls.hours is None:
+            cls.hours = (7 - self.rest_days) * 8
+        return cls(name, rest_days, email)
 
-    def get_email(self):
-        return f"{self.name}@email.com"
+    @classmethod
+    def get_email(cls, name, hours, rest_days):
+        if cls.email is None:
+            cls.email = f"{name}@email.com"
+        return cls(name, hours, rest_days, email)
 
     @classmethod
     def set_hourly_payment(cls, new_hourly_payment):
         cls.hourly_payment = new_hourly_payment
 
+    
     def salary(self):
-        return self.get_hours() * self.hourly_payment
-
-
-# Пример использования класса:
-employee1 = EmployeeSalary("Иванов", rest_days=2)
-print("Имя сотрудника:", employee1.name, "количество выходных:", employee1.rest_days, "зарплата:", employee1.salary())
-
-employee2 = EmployeeSalary("Петров", hours=35, email="petrov@example.com")
-print("Имя сотрудника:", employee2.name, "количество рабочих часов:", employee2.hours, "зарплата:", employee2.salary())
-
-EmployeeSalary.set_hourly_payment(450)
-employee3 = EmployeeSalary("Сидоров")
-print("Имя сотрудника:", employee3.name, "количество рабочих часов:", employee3.hourly_payment, "зарплата:", employee3.salary())
+        return self.hours * self.hourly_payment
+    
+employee1 = EmployeeSalary(name = "Иванов", hours = 40, rest_days=4, email = None)
+print("Имя сотрудника:", employee1.name, "количество выходных:", employee1.rest_days, "зарплата:", employee1.salary(), "email:", employee1.email)
